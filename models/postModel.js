c655fa62f67116814ea3ae954534441decce884c
TRAN QUANG DUNG <tqd0105@gmail.com>
2025-03-13 23:48:39 +0700

    Test image API

--- models/postModel.js
+++ models/postModel.js
@@ -1,6 +1,16 @@
 const posts = [
-    { id: 1, name: "John Doe", caption: "Beautiful sunset!", image: "https://pbs.twimg.com/media/GlyAlI1XkAAcIeP?format=jpg&name=large" },
-    { id: 2, name: "Jane Smith", caption: "Had an amazing trip!", image: "https://via.placeholder.com/150" }
+    { id: 1, name: "John Doe", caption: "Beautiful sunset!", image: "https://raw.githubusercontent.com/tqd0105/SpaceZone---Social-Media/refs/heads/master/src/assets/images/main/Posts/Post1.png" },
+    { id: 2, name: "Jane Smith", caption: "Had an amazing trip!", image: "https://raw.githubusercontent.com/tqd0105/SpaceZone---Social-Media/refs/heads/master/src/assets/images/main/Posts/Post2.png" },
+    { id: 3, name: "Bob Johnson", caption: "Delicious food!", image: "https://via.placeholder.com/150" },
+    { id: 4, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
+    { id: 5, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
+    { id: 6, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
+    { id: 7, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
+    { id: 8, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
+    { id: 9, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
+    { id: 10, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
+    { id: 11, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
+    { id: 12, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
   ];
 
 // Tên, Ảnh, Caption, 
