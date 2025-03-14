17caecf8e3029ae478fe9534f840ef3d33d66e8c
TRAN QUANG DUNG <tqd0105@gmail.com>
2025-03-14 23:17:45 +0700

    Fix: Add server as a normal folder

--- models/postModel.js
+++ models/postModel.js
@@ -1,19 +1,18 @@
 const posts = [
-    { id: 1, name: "John Doe", caption: "Beautiful sunset!", image: "https://raw.githubusercontent.com/tqd0105/SpaceZone---Social-Media/refs/heads/master/src/assets/images/main/Posts/Post1.png" },
-    { id: 2, name: "Jane Smith", caption: "Had an amazing trip!", image: "https://raw.githubusercontent.com/tqd0105/SpaceZone---Social-Media/refs/heads/master/src/assets/images/main/Posts/Post2.png" },
-    { id: 3, name: "Bob Johnson", caption: "Delicious food!", image: "https://via.placeholder.com/150" },
-    { id: 4, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
-    { id: 5, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
-    { id: 6, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
-    { id: 7, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
-    { id: 8, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
-    { id: 9, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
-    { id: 10, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
-    { id: 11, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
-    { id: 12, name: "Alice Brown", caption: "New puppy!", image: "https://via.placeholder.com/150" },
-  ];
+  {
+    id: 1,
+    name: "John Doe",
+    caption: "Beautiful sunset!",
+    image:
+      "https://raw.githubusercontent.com/tqd0105/SpaceZone---Social-Media/refs/heads/master/src/assets/images/main/Posts/Post1.png",
+    avatar: "",
+    privacy: "public",
+    createAt: "12 tháng 3 lúc 15:05",
+    postObject: "Page",
+  },
+  // Thêm các bài viết mới tại đây nếu cần
+];
 
-// Tên, Ảnh, Caption, 
+// Tên, Ảnh, Caption,
 
-  export default posts;
-  
+export default posts;
